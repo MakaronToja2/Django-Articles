766302e5a675bf04521eff4601304d0d1e36d657
Makarontoja2 <wloch.jakub2@gmai.com>
2022-10-26 13:15:43 +0200

    28_stage

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 
@@ -22,4 +23,9 @@ def logout_view(request):
     return render(request, 'Accounts/logout.html', {})
 
 def register_view(request):
-    return render(request, 'Accounts/register.html', {})
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        user_obj = form.save()
+        return redirect('/login')
+    context = {'form':form}
+    return render(request, 'Accounts/register.html', context)
