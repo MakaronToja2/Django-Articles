eeb912dfed97daf84fa8a5bf0cc46a7442f5c0b7
Makarontoja2 <wloch.jakub2@gmai.com>
2022-10-27 14:20:16 +0200

    30.stage

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r9w**)9vucp5rybc#=1)@d)hedcw3)m@&c2w=ng@c9&#l4%=9!'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-r9w**)9vucp5rybc#=1)@d)hedcw3)m@&c2w=ng@c9&#l4%=9!')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOSTS')]
 
 
 # Application definition
